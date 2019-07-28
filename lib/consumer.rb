15126b61b16540c5e77f003d1eb38cb6f2413fca
Andrey Koleshko <ka8725@gmail.com>
2019-07-28 18:12:55 +0300

    potimize memory & CPU

--- lib/consumer.rb
+++ lib/consumer.rb
@@ -8,19 +8,33 @@ def do_smt(row)
 end
 
 OpError = Class.new(StandardError)
-
+conn = Events.connection
 loop do
   failed = Set.new
   fail_key = keys.sample
-  res = Events.connection.exec("select id, name, payload, key from events where processed_at is null")
-  res.each do |row|
-    next if row['key'] && failed.include?(row['key'])
-    Events.connection.transaction do |conn|
-      do_smt(row) and (fail(OpError) if res['key'] == fail_key)
-      conn.exec_params('update events set processed_at = now() where id = $1', [row['id']])
-    rescue OpError
-      puts "failed #{fail_key}"
-      failed << fail_key
+  begin
+    conn.send_query(<<~SQL)
+      select id, name, payload, key from events where processed_at is null
+    SQL
+
+    conn.set_single_row_mode
+
+    loop do
+      res = conn.get_result or break
+      res.each do |row|
+        next if row['key'] && failed.include?(row['key'])
+
+        conn.transaction do |conn|
+          do_smt(row) and (fail(OpError) if res['key'] == fail_key)
+          conn.exec_params('update events set processed_at = now() where id = $1', [row['id']])
+        rescue OpError
+          puts "failed #{fail_key}"
+          failed << fail_key
+        end
+      end
     end
+  rescue PG::NoResultError, PG::UnableToSend
+    puts e
   end
+  sleep 2
 end
